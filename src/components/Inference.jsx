7c06d9161325e518674155817a91dd3368a6c1bd
John Burke <john@crc-dot1x-nat-10-239-174-97.bu.edu>
2024-04-24 19:57:13 -0400

    Implemented inference page

--- src/components/Inference.jsx
+++ src/components/Inference.jsx
@@ -1,49 +1,95 @@
 import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { baseApiUrl, uploadClassEndpoint, uploadProjectEndpoint } from '../config';
+import { baseApiUrl, inferenceEndpoint, uploadClassEndpoint, uploadDataEndpoint, uploadProjectEndpoint } from '../config';
 import axios from 'axios';
 
 const Inference = ({apiToken}) => {
     const {project_name} = useParams();
-    const [classString, setClassString] = useState("");
-    const navigate = useNavigate();
 
-    const handleClassStringChange = (e) => {
-        setClassString(e.target.value);
-    }
+    const [outputInference, setOutputInference] = useState(null);
+    const [base64Image, setBase64Image] = useState("");
+
+    const navigate = useNavigate();
 
     const handleCancel = (e) => {
         navigate("/dashboard")
     }
 
-    const handleSubmit = async (e) => {
+    const handleImageUpload = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            const base64String = reader.result;
+            const base64WithoutPrefix = base64String.split(',')[1];
+            setBase64Image(base64WithoutPrefix);
+        }
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+        console.log(base64Image)
+    }
+
+    const sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds));
+      };
+
+    const handleInfer = async (e) => {
         try {
-            const response = await axios.put(baseApiUrl + uploadClassEndpoint, {
-              project_name: project_name,
-              class_info: classString,
-              api_token: apiToken
+            const response = await axios.post(baseApiUrl + inferenceEndpoint, {
+              image_data: base64Image,
+              api_token: apiToken,
+              project_name: project_name
           });
-            console.log("Here is API response")
-            console.log(response)
+            const mapping = response.data['inference_mapping']
+            while (true) {
+                await sleep(2000)
+                try {
+                    const response = await axios.get(baseApiUrl + inferenceEndpoint + `?inference_mapping=${mapping}`)
+                    if (response.status == 200) {
+                        console.log("Collected inference response.")
+                        setOutputInference(response.data)
+                        break;
+                    }
+                }
+                catch (e) {
+                    console.log("Did not get inference response yet.")
+                    continue;
+                }
+            }
           }
           catch (e) {
             console.log(e.response)
           }
-        navigate("/dashboard")
     }
 
     return (
         <div class="add-class-div">
-            <p>Adding classes for {project_name}</p>
-            <p>Enter classes with no spaces, seperated by the '|' character</p>
-            <input
-                type="text"
-                id="classString"
-                value={classString}
-                onChange={handleClassStringChange}
+            <p>Using {project_name} to make an inference</p>
+            <p>Input an image file: </p>
+            <input 
+                type="file" 
+                accept="image/*" 
+                onChange={handleImageUpload} 
             />
             <button onClick={handleCancel}>Cancel</button>
-            <button onClick={handleSubmit}>Save</button>
+            <button onClick={handleInfer}>Infer</button>
+            {outputInference == null ? <></> :
+            <>
+                {Array.isArray(outputInference) ? <p>Nothing was found inside of the image.</p> : 
+                <>
+                    {console.log(outputInference)}
+                    {outputInference.map((detection, index) => (
+                        <div>
+                            <p>{detection['classification']}</p>
+                            <p>{detection['confidence']}</p>
+                        </div>
+                    ))}
+                </>
+                }
+            </>
+            }
         </div>
     );
 };
